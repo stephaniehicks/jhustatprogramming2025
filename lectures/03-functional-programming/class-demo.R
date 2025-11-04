1c2c4c5d38eb27edea2ec4819e7df6b08bbceaf0
Stephanie Hicks <stephaniechicks@gmail.com>
2025-11-04 13:08:11 -0500

    fixed typos

--- lectures/03-functional-programming/class-demo.R
+++ lectures/03-functional-programming/class-demo.R
@@ -1,3 +1,7 @@
+#### load library
+library(purrr)
+
+
 #### map practice
 tmp_dat <- data.frame(
   x = 1:5,
@@ -8,9 +12,9 @@ map(.x = tmp_dat, .f = mean)
 tmp_dat %>% map(.f = mean)
 
 
-
-#### Arguments 
-# e.g. if you want to pass na.rm=TRUE to mean(), you could use an anonymous function: 
+#### Arguments
+# e.g. if you want to pass na.rm=TRUE to mean(),
+#      you could use an anonymous function:
 x <- list(1:5, c(1:10, NA))
 map_dbl(x, ~ mean(.x, na.rm = TRUE))
 
@@ -18,40 +22,49 @@ map_dbl(x, ~ mean(.x, na.rm = TRUE))
 map_dbl(x, mean, na.rm = TRUE)
 
 
-#### Stratified analyses in R 
+
+
+#### Stratified analyses in R
 unique(mtcars$cyl) # different numbers of cylinders
 
-# This creates a list of three data frames: the cars with 4, 6, and 8 cylinders respectively.
+# This creates a list of three data frames:
+#     the cars with 4, 6, and 8 cylinders respectively.
 by_cyl <- split(mtcars, mtcars$cyl)
 length(by_cyl)
 str(by_cyl)
 
-# fit a linear model to understand how the miles per gallon (`mpg`) associated with the weight (`wt`)
+# fit a linear model to understand how the miles per
+#     gallon (`mpg`) associated with the weight (`wt`)
 lm(mpg ~ wt, data = mtcars)
-
-# fit the model with purrr (returns a list from each lm fit for each cylinder)
-by_cyl |>
-  map(.f = ~ lm(mpg ~ wt, data = .x))
 
 # QUESTION
 # How to extract the second coefficient (i.e. the slope)
 
-# using all observations in mtcars: 
+
+
+# fit the model with purrr (returns a list from each
+#     lm fit for each cylinder)
+by_cyl |>
+  map(.f = ~ lm(mpg ~ wt, data = .x))
+
+# using all observations in mtcars:
 lm.fit <- lm(mpg ~ wt, data = mtcars)
 coef(lm.fit)
 
 # how to do with this with map using stratified analsyes? 
-##  try it out 
+##  try it out
+
+
 
 # in base R with a for loop
-Or, of course, you could use a for loop:
+# Or, of course, you could use a for loop:
 
 slopes <- double(length(by_cyl))
 for (i in seq_along(by_cyl)) {
   model <- lm(mpg ~ wt, data = by_cyl[[i]])
   slopes[[i]] <- coef(model)[[2]]
 }
-slopes 
+slopes
 
 
 #### modify()
@@ -66,7 +79,8 @@ map(df, ~ .x * 2)
 modify(df, ~ .x * 2)
 
 #### walk() and friends
-# when you want to apply a function (e.g. cat(), write_csv(), or ggsave()), but do not return output, try walk and friend
+# when you want to apply a function (e.g. cat(), write_csv(), or ggsave()), 
+#      but do not return output, try walk and friend
 # example: consider saving a dataset
 
 tmp_fldr <- tempdir()
